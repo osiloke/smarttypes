29edd26cb51ad5d5761906a90b458739704b24e9
Timmyt Wilson <hello@smarttypes.org>
2012-04-07 09:02:14 -0400

    blah

--- smarttypes/__init__.py
+++ smarttypes/__init__.py
@@ -33,6 +33,6 @@ default_title = '%s - %s' % (site_name, site_mantra)
 site_description = """
 Smarttypes is an open laboratory for human social network analysis (initially twitter).
 We provide automated tools to pull and store social connections and content, while respecting provider terms of service.
-Our purpose -- to help explore the nuance and beautiful complexity that is human relationships!  
+Our purpose -- to explore the nuance and beautiful complexity that is human relationships!  
 """
 site_description = site_description.strip()
